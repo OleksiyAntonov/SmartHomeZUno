535612b23213791bf4d5e1c53301f238f0439ef5
Sulimenko <ASulimenko@luxoft.com>
2021-01-07 19:59:48 +0100

    Split implementation into separate files

---
+++ src/xmaslights/xmas0000/compatibility.h
@@ -0,0 +1,12 @@
+#ifndef COMPATIBILITY_HEADER
+#define COMPATIBILITY_HEADER
+
+#ifdef _MSC_VER
+#include <stdint.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#endif
+
+#endif
